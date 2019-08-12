6cc4b28995aca93da06bd2f35b861dd55aaadb0e
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-08-13 08:04:53 +0900

    00021

---
+++ 00021/00021/TopViewController.swift
@@ -0,0 +1,49 @@
+//
+//  TopViewController.swift
+//  00021
+//
+//  Created by 酒井典昭 on 2019/08/13.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+
+import PGFramework
+
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    @IBAction func touchedButton(_ sender: UIButton) {
+        let secondViewController = SecondViewController()
+        transitionViewController(from: self, to: secondViewController)
+        animatorManager.navigationType = .slide_push
+        
+    }
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension TopViewController {
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
