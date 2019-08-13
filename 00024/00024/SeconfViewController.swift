7597880b87e9ba72b1e19a07b87f6e1ba0b5a742
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-08-13 17:17:10 +0900

    00024

---
+++ 00024/00024/SeconfViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SeconfViewController.swift
+//  00024
+//
+//  Created by 酒井典昭 on 2019/08/13.
+//  Copyright © 2019 典昭酒井. All rights reserved.
+//
+
+import UIKit
+
+import PGFramework
+
+
+// MARK: - Property
+class SeconfViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension SeconfViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+
+// MARK: - Protocol
+extension SeconfViewController {
+    
+}
+
+// MARK: - method
+extension SeconfViewController {
+    
+}
+
