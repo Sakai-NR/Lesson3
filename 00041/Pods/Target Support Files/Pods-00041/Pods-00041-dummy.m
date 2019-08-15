ad8719581bb38722f844f87e507be57337fcc714
酒井典昭 <sakainoriaki@sakainoriakinoMacBook-puro.local>
2019-08-15 13:38:46 +0900

    00042

---
+++ 00041/Pods/Target Support Files/Pods-00041/Pods-00041-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_00041 : NSObject
+@end
+@implementation PodsDummy_Pods_00041
+@end
---
+++ 00042/Pods/Target Support Files/Pods-00042/Pods-00042-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_00042 : NSObject
+@end
+@implementation PodsDummy_Pods_00042
+@end
